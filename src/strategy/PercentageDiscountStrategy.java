47bffeea57cda7073a068a3e9c6f2d2423276994
AxthonyV <axthonyvincere@gmail.com>
2026-05-23 10:31:01 -0500

    feat(strategy): descuento porcentual sobre el total

---
+++ src/strategy/PercentageDiscountStrategy.java
@@ -0,0 +1,17 @@
+package strategy;
+
+public class PercentageDiscountStrategy {
+    private double percentage;
+
+    public PercentageDiscountStrategy(double percentage) {
+        this.percentage = percentage;
+    }
+    @Override
+    public double applyDiscount(double total) {
+        double descuento = total * percentage / 100;
+        System.out.println("Descuento del " + percentage + "%: -S/ " + descuento);
+        return total - descuento;
+    }
+}
+
+
